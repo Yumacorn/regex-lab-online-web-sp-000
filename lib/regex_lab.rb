38ec8f80677fd339d2b3a45474cac9ebac17b4d1
Ryan Mui <reachryanmui@gmail.com>
2020-02-07 00:09:01 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -16,11 +16,13 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
+state = nil
   if text.scan(/^[A-Z]\[.!?]$/)
-    return true
+    state = true
   else
-    return false
+    state = false
   end
+  return state
 end
 
 def valid_phone_number?(phone)
